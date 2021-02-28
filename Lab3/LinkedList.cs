755834b044a7c8e0865a8deba3144d3366d7e7ad
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-02-28 12:59:28 +0200

    implemented LinkedList<T>

---
+++ Lab3/LinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class LinkedList<T>
+    {
+        public LinkedListNode<T> Head { get; }
+        private LinkedListNode<T> _last;
+
+        public LinkedList(T initialData)
+        {
+            Head = new LinkedListNode<T>(initialData, null);
+            _last = Head;
+        }
+
+        public void Add(T item)
+        {
+            _last.Next = new LinkedListNode<T>(item, null);
+            _last = _last.Next;
+        }
+    }
+}
