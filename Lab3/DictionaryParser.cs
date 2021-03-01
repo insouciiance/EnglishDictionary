d35e9ea562c5933ef83e2d54ac37472c5db2536f
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-03-01 12:00:29 +0200

    added DictionaryParser.cs

---
+++ Lab3/DictionaryParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class DictionaryParser
+    {
+        private readonly string _path;
+
+        public DictionaryParser(string path)
+        {
+            _path = path;
+        }
+
+        public async Task<Hashtable> ParseAsync(Func<string, string> keySelector)
+        {
+            Hashtable table = new Hashtable();
+
+            using (StreamReader reader = new StreamReader(_path))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    table.Add(keySelector(line), line);
+                }
+            }
+
+            return table;
+        }
+    }
+}
